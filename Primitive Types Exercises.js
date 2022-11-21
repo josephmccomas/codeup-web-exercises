9c514909d4db7b317a8915021603c6866feaccc7
Joseph McComas <joseph.mccomas430@gmail.com>
2022-11-21 14:59:37 -0600

    Primitive Types Exercises

--- Primitive Types Exercises.js
+++ Primitive Types Exercises.js
@@ -1,27 +1,27 @@
 // Create a variable that holds a boolean value for each of the following conditions:
 
-let username = 'codeup';
-let password = 'notastrongpassword';
 
-function loginInfo() {
+
+function login() {
+    let username = 'codeup';
+    let password = 'notastrongpassword';
 // the password must be at least 5 characters
     if (password.length < 5) {
         return false;
     }
 // the password must not include the username
-    if (password.includes[username]) {
+    if (password.includes(username)) {
         return false;
     }
     // the username must be no more than 20 characters
-    if (username.length < 20) {
+    if (username.length > 20) {
         return false;
     }
     // neither the username or password can start or end with whitespace
-    if ((username[0] == " ") || (password[0] == " ")) {
+    if ((username(0) == " ") || (password(0) == " ")) {
         return false;
     }
-    console.log(loginInfo);
+    console.log(login);
 }
 
 // ++++++++++++++
-
