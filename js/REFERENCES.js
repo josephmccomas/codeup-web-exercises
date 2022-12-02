03b252406d5d074862c6cabbba1e612bfaceb6a4
Joseph McComas <joseph.mccomas430@gmail.com>
2022-12-02 10:39:43 -0600

    Classwork

--- js/REFERENCES.js
+++ js/REFERENCES.js
@@ -1,8 +1,30 @@
 "use strict"
 
-// for each loop
+/** For-each loop
+ *
+ */
+
 var fruits = ["peach", "apple", "pear"];
 
-fruits.forEach(function(fruit) {
-    console.log(fruit);
+fruits.forEach(function(fruits) {
+    console.log(fruits);
 });
+
+// or
+console.log("------------------------");
+function printFruit(fruits) {
+    console.log(fruits);
+}
+
+fruits.forEach(printFruit);
+
+/** For-in loops
+ *
+ */
+
+console.log("------------------------");
+let i = 1;
+for (const fruit of fruits) {
+    console.log(i + ". " + fruit);
+    i++;
+}
