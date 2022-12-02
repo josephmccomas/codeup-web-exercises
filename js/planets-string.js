810bc0ac5d1ccc05799bdfcc56fb2409c877cac8
Joseph McComas <joseph.mccomas430@gmail.com>
2022-12-02 11:33:26 -0600

    Classwork

--- js/planets-string.js
+++ js/planets-string.js
@@ -10,8 +10,8 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-
-    // console.log(planetsArray);
+planetsArray = planetsString.split("|");
+    console.log(planetsArray);
 
     /**
      * TODO:
