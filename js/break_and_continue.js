c3c5942ad1ece7acabcb6feb9ee8bee16e991284
Joseph McComas <joseph.mccomas430@gmail.com>
2022-12-08 09:36:06 -0600

    Classwork

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,36 +3,42 @@
 /** Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
  Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.*/
 
-// function oddNumber() {
-//     let userInput = prompt("Enter an odd number between 1 and 50");
-//     if (userInput)
-// }
-// oddNumber();
-
-function oddNumEntry() {
-    let userInput = prompt("Enter an odd number between 1 and 50");
-    let result = 0;
-    for (let i = 1; i < 50; i++) {
-        if (Number.isInteger(userInput) === true) {
-            alert("That is not a number, try again");
-            break;
-        }
-        if (userInput < 1 || userInput > 49) {
-            alert("Your number is not in range, try again");
-            break;
-        }
-        if (userInput % 2 === 0) {
-            alert("Your number is not odd, try again");
-            break;
-        }
-        if (i % 2 === 1) {
-            if (i === this.userInput) {
-                { continue; }
-            }
-            result += `Here is an odd number: ${i}`;
-            i++;
-        }
+// Prompt for an odd number 1-50
+    let userInput = prompt("Enter an odd number between 1 and 50")
+// Use a loop w/break statement to continue prompting if input is invalid
+    if (userInput % 2 === 0) {
+        alert("Your number is not odd, try again");
+        userInput = prompt("I said, enter an odd number between 1 and 50");
+    } else if (userInput % 2 === 1) {
+        console.log(userInput)
     }
-    console.log(result);
-}
-oddNumEntry();
+
+// Use a loop & continue stmt to output odds 1-50
+
+// Do not output the user's number
+
+
+
+
+    // let userInput = prompt("Enter an odd number between 1 and 50");
+    // // let result = {};
+    // for (let i = 1; i < 50; i++) {
+    //     if (Number.isInteger(userInput) === false)
+    //         alert("That is not a number, try again");
+    //     break;
+    //
+    //     if (userInput < 1 || userInput > 49)
+    //         alert("Your number is not in range, try again");
+    //     break;
+    //
+    //     if (userInput % 2 === 0)
+    //         alert("Your number is not odd, try again");
+    //     break;
+    //
+    //     if (i % 2 === 1) {
+    //         // if (i === this.userInput) continue;
+    //         result += `Here is an odd number: ${i}`;
+    //         i++;
+    //     }
+    // }
+    // console.log(result);
