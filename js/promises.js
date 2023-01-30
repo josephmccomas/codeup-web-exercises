30ab580982a7ee7f38f1faeecd4cd5cec19212e7
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-01-30 16:29:43 -0600

    Classwork

--- js/promises.js
+++ js/promises.js
@@ -3,15 +3,28 @@
     // Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
     const user = "josephmccomas";
 
-function lastCommit() {
-    return fetch("https://api.github.com/users/" + user + "/events/public", {headers: {'Authorization': GITHUB_API_KEY}})
-    .then(function (result) {
-        return result.json();
-    })
-}
-lastCommit().then(function (result){
-    console.log(result);
-    let mostRecentCommit = result[0];
-    console.log(mostRecentCommit);
-    document.getElementById("commit").innerHTML = mostRecentCommit.toString();
-});
+    function lastCommit() {
+        return fetch("https://api.github.com/users/" + user + "/events/public", {headers: {'Authorization': GITHUB_API_KEY}})
+            .then(function (result) {
+                return result.json();
+            })
+            .then(function (data){
+                data.filter(function (){
+                    return data.length === "Push Event";
+                }[0].created_at)
+            })
+    lastCommit(data){
+        let mostRecentCommit = data.created_at;
+        console.log(mostRecentCommit);
+    };
+
+
+// function wait(ms){
+//     return new Promise(function (resolve, reject){
+//         setTimeout(function (){
+//             resolve("Howdy, I waited " + ms + " ms.");
+//         }, ms);
+//     })
+// }
+//     wait(1000).then((msg) => console.log(msg));
+//     wait(3000).then((msg) => console.log(msg));
